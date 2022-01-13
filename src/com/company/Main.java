78510a798854364481d736f647dcd0c3ce70a36f
za <1205038296@qq.com>
2022-01-14 02:22:34 +0800

    第二次提交

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
 	Student student = new Student();
 	Object obj = new Object();
-	System.out.println(student.getClass().cast(obj));
 
     }
 }
