e2542b0d0e8ad6f1c0b1f377c6703e7f70cad5a7
za <1205038296@qq.com>
2022-01-14 02:42:05 +0800

    第二次提交

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
 	Student student = new Student();
 	Object obj = new Object();
-
+    System.out.println("原来是第一个测试");
     }
 }
---
+++ src/com/company/Shool.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Shool {
+    private String num;
+    private String s_name;
+    private String s_address;
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getS_name() {
+        return s_name;
+    }
+
+    public void setS_name(String s_name) {
+        this.s_name = s_name;
+    }
+
+    public String getS_address() {
+        return s_address;
+    }
+
+    public void setS_address(String s_address) {
+        this.s_address = s_address;
+    }
+}
